41fd2950ff08813aef7932256bfeacbe6734a432
Dwij Sukeshkumar Sheth <dwijhariket@gmail.com>
2017-09-17 14:46:43 +0530

    Add files via upload

---
+++ Flight details.php
@@ -0,0 +1,66 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="Front with CSS.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-orange.min.css" />
+    <script src="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <title>
+        Booking Confirmed
+    </title>
+</head>
+
+<body background="airplane.jpg">
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <header class="mdl-layout__header">
+            <div class="mdl-layout__header-row">
+                <!-- Title -->
+                <span class="mdl-layout-title">Booking Confirmed</span>
+                <!-- Add spacer, to align navigation to the right -->
+                <div class="mdl-layout-spacer"></div>
+                <!-- Navigation. We hide it in small screens. -->
+                <nav class="mdl-navigation mdl-layout--large-screen-only">
+                    <a class="mdl-navigation__link" href="home.php">Home</a>
+                    <a class="mdl-navigation__link" href="form.php">Signup</a>
+                    <a class="mdl-navigation__link" href="front.php">Login</a>
+                    <a class="mdl-navigation__link" href="Aboutus.php">About Us</a>
+                </nav>
+            </div>
+        </header>
+        <div class="mdl-layout__drawer">
+            <span class="mdl-layout-title">Home</span>
+            <nav class="mdl-navigation">
+                <a class="mdl-navigation__link" href="home.php">Home</a>
+                <a class="mdl-navigation__link" href="form.php">Signup</a>
+                <a class="mdl-navigation__link" href="front.php">Login</a>
+                <a class="mdl-navigation__link" href="Aboutus.php">About Us</a>
+            </nav>
+        </div>
+        <main class="mdl-layout__content">
+            <div class="page-content">
+                <br><br><br><br>
+                <center>
+
+                    <div class="demo-card-wide mdl-card mdl-shadow--16dp" style="width:900px">
+                        <div class="mdl-shadow--16dp" style="background-color:#000000"><br>
+                            <h1 style="color:white">
+                                Your Flight has been booked
+                            </h1>
+                        </div>
+                        <br> <b style="padding-left:0px">Flight No: Date of Journey:</b><br><br> From: To:
+                        <br><br> Dep Time: Arr Time:<br><br> PNR No.:<br><br><br>
+                        <div class="mdl-shadow--16dp" style="background-color:#000000"><br>
+                            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                        <div class="mdl-typography--subtitle" style="color:black;font-weight:bolder">Book Another</div>
+                    </button>
+                            <br><br>
+                        </div>
+
+                    </div>
+                </center>
+            </div>
+        </main>
+</body>
+
+</html>
