8620d868f657279159591def13a6745a9b9c5c62
Dwij Sukeshkumar Sheth <dwijhariket@gmail.com>
2017-10-29 06:21:25 -0700

    Add files via upload

--- dbmsproject.sql
+++ dbmsproject.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 25, 2017 at 02:33 PM
+-- Generation Time: Oct 29, 2017 at 02:20 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 7.0.9
 
@@ -47,11 +47,26 @@ INSERT INTO `adminlogin` (`AID`, `Username`, `Password`) VALUES
 --
 
 CREATE TABLE `booking` (
-  `F_no` varchar(6) NOT NULL,
-  `CID` varchar(6) NOT NULL,
-  `TicketID` varchar(6) NOT NULL,
-  `P_name` int(11) NOT NULL
+  `TicketID` int(6) NOT NULL,
+  `F_no` varchar(5) NOT NULL,
+  `Source` varchar(30) NOT NULL,
+  `Destination` varchar(30) NOT NULL,
+  `Cid` int(5) NOT NULL,
+  `Date` date NOT NULL,
+  `Fare` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `booking`
+--
+
+INSERT INTO `booking` (`TicketID`, `F_no`, `Source`, `Destination`, `Cid`, `Date`, `Fare`) VALUES
+(3, 'V123', 'Delhi', 'Bangalore', 3, '2017-10-23', '4500'),
+(4, 'V194', 'Bangalore', 'Delhi', 3, '2017-10-11', '4100'),
+(5, 'V289', 'Mumbai', 'Delhi', 1, '2017-10-29', '2530'),
+(6, 'V12', 'Delhi', 'Mumbai', 1, '2017-11-23', '6464'),
+(7, 'V289', 'Mumbai', 'Delhi', 1, '2017-10-29', '2530'),
+(8, 'V12', 'Delhi', 'Mumbai', 1, '2017-11-23', '6464');
 
 -- --------------------------------------------------------
 
@@ -80,7 +95,9 @@ CREATE TABLE `customerlogin` (
 --
 
 INSERT INTO `customerlogin` (`Customer_id`, `First_name`, `Last_name`, `email_id`, `DOB`, `Gender`, `Nationality`, `Occupation`, `Address`, `City`, `State`, `Mobile_no`, `Password`) VALUES
-(1, 'Yash', 'Jain', 'yashjain.2016@vitstudent.ac.in', '1998-08-28', 'Male', 'Indian', 'Student', 'My Home Address', 'Hissar', 'Haryana', '9467533303', 'jains');
+(1, 'Yash', 'Jain', 'yashjain.2016@vitstudent.ac.in', '1998-08-28', 'Male', 'Indian', 'Student', 'My Home Address', 'Hissar', 'Haryana', '9467533303', 'jains'),
+(3, 'Dwij', 'Sheth', 'dwijhariket@gmail.com', '1998-12-28', 'Male', 'Indian', 'Student', 'Room No-318 A Block', 'Chennai', 'Tamil Nadu', '7889037976', 'dwij'),
+(4, 'Shantanu', 'Singh', 'shaneya.singh13@gmail.com', '1998-12-28', 'Male', 'Indian', 'Student', 'Room No-318 A Block', 'Chennai', 'Tamil Nadu', '9876543210', 'shantanu');
 
 -- --------------------------------------------------------
 
@@ -89,25 +106,30 @@ INSERT INTO `customerlogin` (`Customer_id`, `First_name`, `Last_name`, `email_id
 --
 
 CREATE TABLE `flight` (
+  `TID` int(11) NOT NULL,
   `F_no` varchar(6) NOT NULL,
   `Origin` varchar(30) NOT NULL,
   `Destination` varchar(30) NOT NULL,
-  `Dep` int(4) NOT NULL,
-  `Arr` int(4) NOT NULL,
-  `Fare` int(5) NOT NULL,
-  `Date` date NOT NULL
+  `Dep` varchar(10) NOT NULL,
+  `Arr` varchar(10) NOT NULL,
+  `Fare` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `flight`
 --
 
-INSERT INTO `flight` (`F_no`, `Origin`, `Destination`, `Dep`, `Arr`, `Fare`, `Date`) VALUES
-('V678', 'Chennai', 'Mumbai', 650, 830, 2500, '2017-09-21'),
-('V747', 'Chennai', 'Delhi', 2000, 2230, 3800, '2017-09-20'),
-('V757', 'Delhi', 'Chennai', 1630, 1900, 3800, '2017-09-20'),
-('V767', 'Chennai ', 'Mumbai', 1500, 1645, 4200, '2017-09-20'),
-('V777', 'Mumbai', 'Chennai', 1700, 1840, 4500, '2017-09-20');
+INSERT INTO `flight` (`TID`, `F_no`, `Origin`, `Destination`, `Dep`, `Arr`, `Fare`) VALUES
+(1, 'V12', 'Delhi', 'Mumbai', '04:06', '06:44', 6464),
+(2, 'V123', 'Delhi', 'Bangalore', '12:00', '14:45', 4500),
+(3, 'V143', 'Mumbai', 'Kolkatta', '14:45', '17:00', 5500),
+(4, 'V194', 'Bangalore', 'Delhi', '20:00', '22:45', 4100),
+(5, 'V201', 'Chennai', 'Bangalore', '15:40', '16:30', 1465),
+(6, 'V289', 'Mumbai', 'Delhi', '00:00', '02:00', 2530),
+(7, 'V747', 'Chennai', 'Delhi', '2000', '2230', 3800),
+(8, 'V757', 'Delhi', 'Chennai', '1630', '1900', 3800),
+(9, 'V767', 'Chennai ', 'Mumbai', '1500', '1645', 4200),
+(10, 'V789', 'Kolkatta', 'Bangalore', '14:55', '16:10', 7800);
 
 --
 -- Indexes for dumped tables
@@ -124,6 +146,7 @@ ALTER TABLE `adminlogin`
 -- Indexes for table `booking`
 --
 ALTER TABLE `booking`
+  ADD PRIMARY KEY (`TicketID`),
   ADD UNIQUE KEY `TicketID` (`TicketID`);
 
 --
@@ -139,17 +162,27 @@ ALTER TABLE `customerlogin`
 -- Indexes for table `flight`
 --
 ALTER TABLE `flight`
-  ADD PRIMARY KEY (`F_no`);
+  ADD PRIMARY KEY (`TID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `TicketID` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
 -- AUTO_INCREMENT for table `customerlogin`
 --
 ALTER TABLE `customerlogin`
-  MODIFY `Customer_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Customer_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `flight`
+--
+ALTER TABLE `flight`
+  MODIFY `TID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
